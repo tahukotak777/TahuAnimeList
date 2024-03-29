92f7d12ebd640dc747f3f5c20d56965d358873ad
farel nanda s <farelnanda11@gmail.com>
2024-03-29 09:06:01 +0700

    membetulkan font

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,11 +1,11 @@
-import { Gabarito } from "next/font/google";
+import { Inter } from 'next/font/google'
 import "./globals.css";
 import NavBar from "@/components/NavBar";
 import NavBarBottom from "@/components/NavBarBottom";
 import { SpeedInsights } from "@vercel/speed-insights/next";koko777
 
 
-const gabarito = Gabarito({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: "TahuAnimeList",
@@ -15,7 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${gabarito.className} bg-main-dark`}>
+      <body className={`${inter.className} bg-main-dark`}>
         <NavBar />
         <div className="mb-14">{children}</div>
         <NavBarBottom />
